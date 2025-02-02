84d2e759a2decc9ff777aefceac30dfaa53fc91c
An-Haeeun-02 <godms531@naver.com>
2025-02-02 20:41:56 +0900

    스프링 핵심 원리 - 기본편 - 섹션4. 스프링 핵심 원리 이해2 - 객체지향 원리 적용(스프링 전환 후)

--- Spring 기본편/core/src/main/java/hello/core/AppConfig.java
+++ Spring 기본편/core/src/main/java/hello/core/AppConfig.java
@@ -1,31 +1,35 @@
 package hello.core;
 
 import hello.core.Discount.DiscountPolicy;
-import hello.core.Discount.FixDiscountPolicy;
 import hello.core.Discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
-import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 //무언가 변경사항이 있을때 이 구성영역만. 즉, 클라이언트 코드를 변경하지 않고 바꾼다.
+@Configuration //설정정보, 구성정보
 public class AppConfig { //구현객체 생성
-
+    @Bean
     public MemberService memberService(){//생성자 주입 MemberService역황
         return new MemberServiceImpl(memberRepository());//구현
     }
 
-    private MemberRepository memberRepository() {//Memberrepository 역활
+    @Bean
+    public MemberRepository memberRepository() {//Memberrepository 역활
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService(){//OrderService 역할
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();  //할인정책 변경
--- Spring 기본편/core/src/main/java/hello/core/MemberApp.java
+++ Spring 기본편/core/src/main/java/hello/core/MemberApp.java
@@ -3,14 +3,19 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
 
         //MemberService memberService = new MemberServiceImpl();
+
+        //스프링은 여기서 시작. @Been을 다 관리함, annotation 기반 config함
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);//(이름_보통메서드명. 타입)
 
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
